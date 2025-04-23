adba02b169f65a7dec15389ed99805aa8fd09eae
Yayopdf <120620117+Yayopdf@users.noreply.github.com>
2025-04-23 03:02:47 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Inicia Dropbear en el puerto 22
-/usr/sbin/dropbear -E -F -p 22 &
-
-# Inicia Dropbear adicional para WebSocket (puerto 80)
-/usr/sbin/dropbear -E -F -p 80 &
+# Iniciar Dropbear para SSH en el puerto 22 y WebSocket en el puerto 80
+/usr/sbin/dropbear -E -F -p 22 -p 80 &
 
 # Mensaje en consola
 echo "Servidor SSH + Dropbear + WebSocket corriendo..."
